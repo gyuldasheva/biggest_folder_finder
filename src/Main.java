ae24908037900ea8c60a03f5b5354d7a851666b3
Gyt <gulya.yuldasheva@gmail.com>
2025-08-01 11:24:56 +0500

    dev

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,21 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
     public static void main(String[] args) {
 
-        String folderPath = "D:\\Doc\\Desktop\\Installer";
+        String folderPath = "D:\\Doc\\Desktop";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+//        System.out.println(getFolderSize(file) + "B");
 
-        long start = System.currentTimeMillis();
+//        long start = System.currentTimeMillis();
 
         FolderSizeCalculator calculator = new FolderSizeCalculator(file);
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
         System.out.println(size);
 
-        long duration = System.currentTimeMillis() - start;
-        System.out.println(duration + " ms");
+//        long duration = System.currentTimeMillis() - start;
+//        System.out.println(duration + " ms");
+//
+        System.out.println(getReadableSize(size));
     }
 
     public static long getFolderSize(File folder) {
@@ -29,5 +31,16 @@ public class Main {
             sum += getFolderSize(file);
         }
         return sum;
+    }
+
+    public static String getReadableSize(long size) {
+        int oneBite = 1024;
+        return size / oneBite + "Kb";
+    }
+
+    public static long getSizeFromHumanReadable(String size) {
+        int oneBite = 1024;
+
+        return 0;
     }
 }
